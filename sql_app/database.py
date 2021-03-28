90ce74e83103a1fa240262d67e8be2cbd80ca7d7
Le Hai Thanh <simatgai01@mail.com>
2021-03-29 02:06:55 +0700

    update database

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,12 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+import os
+import urllib
+host_server = os.environ.get('host_server', 'satao.db.elephantsql.com')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
+database_name = os.environ.get('database_name', 'xsboezwp')
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'xsboezwp')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'leMY5iTxf9ABe94Phic37RZPmkdiS-Ca')))
+ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
+SQLALCHEMY_DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgres://xsboezwp:leMY5iTxf9ABe94Phic37RZPmkdiS-Ca@satao.db.elephantsql.com:5432/xsboezwp"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL ,
+    pool_size = 3,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
