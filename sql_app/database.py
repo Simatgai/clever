9baa288a053f90a364e5dd6d81880f59255669b5
Le Hai Thanh <simatgai01@mail.com>
2021-03-30 14:42:58 +0700

    first commit

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 import urllib
-host_server = os.environ.get('host_server', 'satao.db.elephantsql.com')
+host_server = os.environ.get('host_server', 'bdarnyoo1zvhftbmglvs-postgresql.services.clever-cloud.com')
 db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
-database_name = os.environ.get('database_name', 'xsboezwp')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'xsboezwp')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'leMY5iTxf9ABe94Phic37RZPmkdiS-Ca')))
+database_name = os.environ.get('database_name', 'bdarnyoo1zvhftbmglvs')
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'uuhihfpokyjr5u90msul')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'PLxR9DbmSZBFXqdg3qQf')))
 ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
 SQLALCHEMY_DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
